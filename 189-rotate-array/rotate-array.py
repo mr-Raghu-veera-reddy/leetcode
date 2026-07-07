f16b206e588936682a4270f429154d2ecf6ff89d
Raghu <raghuveerareddy642@gmail.com>
2026-07-07 18:28:13 +0530

    Time: 12 ms (15.49%) | Memory: 23.3 MB (43.81%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -13,18 +13,18 @@ class Solution(object):
 
         k=k%length 
 
-        # If k is 0, the array doesn't need to change at all!
-        if k != 0:
-            nums[:] = nums[-k:] + nums[:-k]
+        # # If k is 0, the array doesn't need to change at all!
+        # if k != 0:
+        #     nums[:] = nums[-k:] + nums[:-k]
 
-    #     self.reverse(nums,0,length-1)
+        self.reverse(nums,0,length-1)
 
-    #     self.reverse(nums,0,k-1)
+        self.reverse(nums,0,k-1)
 
-    #     self.reverse(nums,k,length-1)
+        self.reverse(nums,k,length-1)
 
-    # def reverse(self,nums,start,end):
-    #     while start<end:
-    #         nums[start],nums[end]=nums[end],nums[start]
-    #         start+=1
-    #         end-=1
+    def reverse(self,nums,start,end):
+        while start<end:
+            nums[start],nums[end]=nums[end],nums[start]
+            start+=1
+            end-=1
