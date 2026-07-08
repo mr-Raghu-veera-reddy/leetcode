52d95a24bfd1081033a4c558eba069deaa47d550
Raghu <raghuveerareddy642@gmail.com>
2026-07-08 10:37:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.02%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,12 +7,11 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        # Set pointers to the last valid elements and the last empty slot
+
         p1 = m - 1
         p2 = n - 1
         p = m + n - 1
-        
-        # Merge backwards, taking the largest element each time
+
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
@@ -21,30 +20,29 @@ class Solution(object):
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
-            
-        # If nums2 still has elements left, copy them over.
-        # (If nums2 is empty or exhausted, this loop just won't run.
-        # If nums1 has elements left, they are already perfectly in place!)
-        while p2 >= 0:
+        
+        while p2>=0:
+         
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
